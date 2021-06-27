3ceb4a1f378a5b90308c30a56e528ce4e5538b2f
adriancast <hello.adriancast@gmail.com>
2021-06-27 16:32:52 +0200

    Serve media files when debug is True

--- {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # When you are using the dev docker-compose, you need to serve the media files somehow
+    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + urlpatterns
