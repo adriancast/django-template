d0da6c7f4268d001d83b5bb1f2796e9081cc5a6b
adriancast <hello.adriancast@gmail.com>
2021-07-03 13:46:04 +0200

    Fix missing sentry import in django settings

--- {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/app/{{cookiecutter.project_name}}/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+{% if cookiecutter.add_sentry == 'yes' %}
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+{% endif %}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
